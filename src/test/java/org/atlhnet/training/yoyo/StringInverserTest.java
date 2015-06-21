4b8070c4ecfda5d1b0d2ef9624b33bd07ebafc51
Rowan PUTOA <rowan.putoa@gmail.com>
2015-06-21 19:39:33 +0200

    RPU - Add method addInverseString

--- src/test/java/org/atlhnet/training/yoyo/StringInverserTest.java
+++ src/test/java/org/atlhnet/training/yoyo/StringInverserTest.java
@@ -11,14 +11,18 @@ public class StringInverserTest {
 
     private final String toInverse1 = "Bob";
     private final String expected1 = "boB";
+    private final String expectedAddInverseString1="BobboB";
 
     private final String toInverse2 = "TestLol";
     private final String expected2 = "loLtseT";
+    private final String expectedAddInverseString2="TestLolloLtseT";
 
     private final String toInverse3 = "Ceciestunemaison";
     private final String expected3 = "nosiamenutseiceC";
+    private final String expectedAddInverseString3="CeciestunemaisonnosiamenutseiceC";
 
     private final String expectedEmpty = "Yoyo";
+    private final String expectedAddInverseStringEmpty="Yoyo";
 
     private StringInverser stringInverser = new StringInverser();
     @Test
@@ -40,10 +44,29 @@ public class StringInverserTest {
         assertNull(assertIsNull);
 
         assertEquals(expected1, assert1);
-        assertEquals(expected2, assert1);
-        assertEquals(expected3, assert1);
+        assertEquals(expected2, assert2);
+        assertEquals(expected3, assert3);
         assertEquals(expectedEmpty, assertEmpty);
+    }
 
+    @Test
+    public void addInverseString() {
+        String result1 = stringInverser.addInverseString(toInverse1);
+        String result2 = stringInverser.addInverseString(toInverse2);
+        String result3 = stringInverser.addInverseString(toInverse3);
+        String resultEmpty = stringInverser.addInverseString("");
+        String resultnull = stringInverser.addInverseString(null);
+
+        assertNotNull(result1);
+        assertNotNull(result2);
+        assertNotNull(result3);
+        assertNotNull(resultEmpty);
+        assertNull(resultnull);
+
+        assertEquals(expectedAddInverseString1, result1);
+        assertEquals(expectedAddInverseString2, result2);
+        assertEquals(expectedAddInverseString3, result3);
+        assertEquals(expectedAddInverseStringEmpty, resultEmpty);
 
     }
 }
